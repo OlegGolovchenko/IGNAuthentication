09298248bdc7b0b1e1a500c28855826472e30360
Oleg Golovchenko <IgRoK_be@hotmail.com>
2020-02-19 22:33:22 +0100

    fixed problem with data project

---
+++ IGNAuthentication.Data/MsSqlDataProvider.cs
@@ -0,0 +1,71 @@
+﻿using IGNAuthentication.Domain.DataRelated;
+using IGNAuthentication.Domain.Interfaces;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace IGNAuthentication.Data
+{
+    public class MsSqlDataProvider : IDataProvider
+    {
+        private string _connectionString;
+
+        public MsSqlDataProvider(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public void CreateTableIfNotExists(string tableName, IEnumerable<TableField> columns)
+        {
+            var query = new StringBuilder();
+            query.Append("IF NOT EXISTS(SELECT * FROM sysobjects WHERE name='");
+            query.Append(tableName);
+            query.Append("' and xtype='U')");
+            query.AppendLine();
+            query.Append("CREATE TABLE ");
+            query.Append(tableName);
+            query.Append("(");
+            foreach (var col in columns)
+            {
+                query.Append(col.Name);
+                query.Append(" ");
+                query.Append(col.Type);
+                query.Append(" ");
+                query.Append(col.CanHaveNull ? "null" : "not null");
+                if (!string.IsNullOrEmpty(col.DefValue))
+                {
+                    query.Append(" default ");
+                    query.Append(col.DefValue);
+                }
+                if (col.Generated)
+                {
+                    query.Append(" IDENTITY(1,1)");
+                }
+                query.Append(", ");
+            }
+            query.Append(");");
+            ExecuteNonQuery(query.ToString());
+        }
+
+        public void ExecuteNonQuery(string query)
+        {
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+                var command = new SqlCommand(query, connection);
+                command.ExecuteNonQuery();
+                connection.Close();
+            }
+        }
+
+        public DbDataReader ExecuteReader(string query, out DbConnection connection)
+        {
+            connection = new SqlConnection(_connectionString);
+            connection.Open();
+            var command = new SqlCommand(query, (SqlConnection)connection);
+            DbDataReader result = command.ExecuteReader();
+            return result;
+        }
+    }
+}
---
+++ IGNAuthentication.Data/MySqlDataProvider.cs
@@ -0,0 +1,88 @@
+﻿using IGNAuthentication.Domain.DataRelated;
+using IGNAuthentication.Domain.Interfaces;
+using MySql.Data.MySqlClient;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Text;
+using System.Linq;
+
+namespace IGNAuthentication.Data
+{
+    public class MySqlDataProvider : IDataProvider
+    {
+        private string _connectionString;
+
+        public MySqlDataProvider(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public void CreateTableIfNotExists(string tableName, IEnumerable<TableField> columns)
+        {
+            var query = new StringBuilder();
+            query.Append("CREATE TABLE IF NOT EXISTS ");
+            query.Append(tableName);
+            query.Append("(");
+            var last = columns.LastOrDefault();
+            foreach (var col in columns)
+            {
+                query.Append(col.Name);
+                query.Append(" ");
+                query.Append(col.Type);
+                query.Append(" ");
+                query.Append(col.CanHaveNull ? "null" : "not null");
+                if (!string.IsNullOrEmpty(col.DefValue))
+                {
+                    query.Append(" default ");
+                    var defValue = col.DefValue;
+                    if (col.Type == "bit")
+                    {
+                        defValue = col.DefValue == "'true'" ? "1" : "0";
+                    }
+                    query.Append(defValue);
+                }
+                if (col.Generated)
+                {
+                    query.Append(" auto_increment");
+                }
+                if (col != last)
+                {
+                    query.Append(", ");
+                }
+            }
+            var pk = columns.SingleOrDefault(fld => fld.Primary);
+            if (pk != null)
+            {
+                query.Append(", constraint ");
+                query.Append("pk_");
+                query.Append(tableName);
+                query.Append(pk.Name);
+                query.Append(" primary key(");
+                query.Append(pk.Name);
+                query.Append(")");
+            }
+            query.Append(");");
+            ExecuteNonQuery(query.ToString());
+        }
+
+        public void ExecuteNonQuery(string query)
+        {
+            using (var connection = new MySqlConnection(_connectionString))
+            {
+                connection.Open();
+                var command = new MySqlCommand(query, connection);
+                command.ExecuteNonQuery();
+                connection.Close();
+            }
+        }
+
+        public DbDataReader ExecuteReader(string query, out DbConnection connection)
+        {
+            connection = new MySqlConnection(_connectionString);
+            connection.Open();
+            var command = new MySqlCommand(query, (MySqlConnection)connection);
+            MySqlDataReader result = command.ExecuteReader();
+            return result;
+        }
+    }
+}
