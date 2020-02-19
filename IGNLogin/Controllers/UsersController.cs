d216e84e33f07e0affcfe139adf7f903ebde5c50
Oleg Golovchenko <IgRoK_be@hotmail.com>
2020-02-19 21:15:50 +0100

    added user controller

---
+++ IGNLogin/Controllers/UsersController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using IGNLogin.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IGNLogin.Controllers
+{
+    [Route("api/user")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private IGNAuthentication.Domain.ServiceProvider _services;
+        public UsersController(IGNAuthentication.Domain.ServiceProvider services)
+        {
+            _services = services;
+        }
+        [HttpPost]
+        public IActionResult GetOrCreateUser([FromQuery]string email)
+        {
+            try
+            {
+                var user = _services.GetUserService().GetOrCreateUser(email);
+                if(user != null)
+                {
+                    return Ok(new UserModel
+                    {
+                        Id = user.Id,
+                        Email = user.Email,
+                        IsActive = user.IsActive
+                    });
+                }
+                return NotFound();
+            }
+            catch(Exception e)
+            {
+                return BadRequest(e);
+            }
+        }
+    }
+}
---
+++ IGNLogin/Models/UserModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IGNLogin.Models
+{
+    public class UserModel
+    {
+        public string Email { get; set; }
+
+        public long Id { get; set; }
+
+        public bool IsActive { get; set; }
+
+    }
+}
